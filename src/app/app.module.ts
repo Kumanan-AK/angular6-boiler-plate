3ee2bf9effa16b771c1d934dd3a47ee27cc0998f
kumananak <kumananak@kumananaks-MacBook-Pro.local>
2018-05-27 17:42:20 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule, formatNumber } from '@angular/common';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -10,6 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
